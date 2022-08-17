0ae26c3ab8e5779d0cce9d9132e1a1d39db5f68e
Pedro Ferreira <noobiept@users.noreply.github.com>
2022-08-17 23:32:02 +0200

    Move the code files to the 'source' directory.

---
+++ source/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+
+namespace NotesApp
+    {
+    public partial class App : Application
+        {
+        private void applicationSessionEnding( object sender, SessionEndingCancelEventArgs e )
+            {
+            var notesWindow = (NotesWindow) this.MainWindow;
+
+            notesWindow.saveToDisk();
+            }
+        }
+    }
